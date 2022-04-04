70fc0c8bc7e687a72bb2a5a66f0058e2a7c321c3
warren roque <warrenroque17@gmail.com>
2022-04-04 18:54:39 +0000

    edit code

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,12 +1,12 @@
 CREATE TABLE cohorts (
-  id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   start_date DATE,
   end_date DATE
 );
 
 CREATE TABLE students (
-  id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   email VARCHAR(255),
   phone VARCHAR(32),
