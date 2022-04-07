3de747a695d86b1a7f715eaa14ad3aa243f9114e
warren roque <warrenroque17@gmail.com>
2022-04-07 01:02:27 +0000

    query practice

---
+++ 4_queries/10_most_confusing_assignments.sql
@@ -0,0 +1,5 @@
+SELECT assignments.id, day, chapter, name, count(assistance_requests) as total_requests
+FROM assignments
+JOIN assistance_requests ON assignment_id = assignments.id
+GROUP BY assignments.id
+ORDER BY total_requests DESC;
